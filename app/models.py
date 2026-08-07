8231e8ebaba01329b28ea8b756d14606420adb0d
Jenisha Rai <jenisha.rai.2224@gmail.com>
2026-08-07 06:12:11 +0545

    Configure MySQL and create User model

---
+++ app/models.py
@@ -0,0 +1,12 @@
+from . import db
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    full_name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+
+    def __repr__(self):
+        return f"<User {self.email}>"
